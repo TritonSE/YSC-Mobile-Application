0d045c3ae3f30359132ce6f74c6a0a006a1bfa8e
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2022-01-25 18:57:05 -0500

    fixing linter errors for forgotpassword.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useFonts, Roboto_400Regular} from '@expo-google-fonts/roboto';
-import LogInScreen from './src/screens/LogInScreen';
-import SplashScreegit n from './src/screens/SplashScreen';
-import LoginScreen2 from './src/screens/LoginScreen2';
-import ForgotPassword from './src/screens/ForgotPassword';
+import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
 
-
+import ForgotPassword from "./src/screens/ForgotPassword";
+import LogInScreen from "./src/screens/LogInScreen";
+import LoginScreen2 from "./src/screens/LoginScreen2";
+import SplashScreen from "./src/screens/SplashScreen";
 
 const Stack = createNativeStackNavigator();
-
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash Screen"> 
-        <Stack.Screen name="Splash Screen 2" component={SplashScreen}  options={{headerShown:false}}/>
-        <Stack.Screen name="Login page" component={LogInScreen}  options={{headerShown:false}}/>
-        <Stack.Screen name="Login page 2" component={LoginScreen2}  options={{headerShown:false}} />
-        <Stack.Screen name="forgot password screen" component={ForgotPassword}  options={{headerShown:false}} />
-
+      <Stack.Navigator initialRouteName="Splash Screen">
+        <Stack.Screen
+          name="Splash Screen 2"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Login page" component={LogInScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Login page 2"
+          component={LoginScreen2}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="forgot password screen"
+          component={ForgotPassword}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -29,11 +38,10 @@ function App() {
 
 export default App;
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- mobile/src/screens/ForgotPassword.tsx
+++ mobile/src/screens/ForgotPassword.tsx
@@ -1,6 +1,81 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, Pressable, TextInput, SafeAreaView } from "react-native";
 
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#FFFFFF",
+    },
+  
+    TextHeader: {
+      color: "#000000",
+      fontFamily: "Roboto",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: 16,
+      // alignContent: 'left',
+    },
+  
+    characterImage: {
+      width: 234,
+      height: 395,
+    },
+  
+    textInputField: {
+      fontSize: 16,
+      fontFamily: "Roboto",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      // position: 'absolute',
+      borderRadius: 3,
+      // background: '#FFFFFF',
+      width: 327,
+      height: 37,
+      // top: 19,
+      // bottom: 3,
+      borderWidth: 1,
+      borderColor: "#000000",
+      alignContent: "center",
+      left: "0%",
+      right: "0%",
+      top: "33.93%",
+    },
+  
+    // same code from LoginInScreen.jsx
+    ResetPasswordButton: {
+      backgroundColor: "#96C957",
+      width: 327,
+      height: 52,
+      borderRadius: 3,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 20,
+      // flex: 1,
+      // order: 0,
+      // flexGrow: 0,
+    },
+  
+    // reusing same code from LoginInScreen.jsx
+    ResetPasswordButtonText: {
+      position: "absolute",
+      fontSize: 18,
+      fontFamily: "Roboto",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      lineHeight: 21,
+      /* identical to box height */
+  
+      // textAlign: 'center',
+      // textTransform: 'capitalize'
+    },
+  });
+
+  
+  
 function ForgotPassword() {
   const [currentPasswordText, changedCurrentPasswordText] = React.useState("Example Padding");
   const [newPasswordText, changednewPasswordText] = React.useState("Example Padding");
@@ -63,74 +138,3 @@ function ForgotPassword() {
 }
 
 export default ForgotPassword;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#FFFFFF",
-  },
-
-  TextHeader: {
-    color: "#000000",
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: 16,
-    // alignContent: 'left',
-  },
-
-  characterImage: {
-    width: 234,
-    height: 395,
-  },
-
-  textInputField: {
-    fontSize: 16,
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    // position: 'absolute',
-    borderRadius: 3,
-    // background: '#FFFFFF',
-    width: 327,
-    height: 37,
-    // top: 19,
-    // bottom: 3,
-    borderWidth: 1,
-    borderColor: "#000000",
-    alignContent: "center",
-    left: "0%",
-    right: "0%",
-    top: "33.93%",
-  },
-
-  // same code from LoginInScreen.jsx
-  ResetPasswordButton: {
-    backgroundColor: "#96C957",
-    width: 327,
-    height: 52,
-    borderRadius: 3,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    // flex: 1,
-    // order: 0,
-    // flexGrow: 0,
-  },
-
-  // reusing same code from LoginInScreen.jsx
-  ResetPasswordButtonText: {
-    position: "absolute",
-    fontSize: 18,
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    lineHeight: 21,
-    /* identical to box height */
-
-    // textAlign: 'center',
-    // textTransform: 'capitalize'
-  },
-});
