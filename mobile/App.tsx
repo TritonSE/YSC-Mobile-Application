728e510b8694a34d60d834c9e0e8088e468a1a4a
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2022-01-25 18:53:01 -0500

    uncommenting styles for app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,56 +1,27 @@
-import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts, Roboto_400Regular} from '@expo-google-fonts/roboto';
+import LogInScreen from './src/screens/LogInScreen';
+import SplashScreegit n from './src/screens/SplashScreen';
+import LoginScreen2 from './src/screens/LoginScreen2';
+import ForgotPassword from './src/screens/ForgotPassword';
 
-import ForgotPassword from "./src/screens/ForgotPassword";
-import LogInScreen from "./src/screens/LogInScreen";
-import LoginScreen2 from "./src/screens/LoginScreen2";
-import { RootStackParamList } from "./src/screens/RootStackParams";
-import SplashScreen from "./src/screens/SplashScreen";
 
-// export type RootStackParamList = {
-//   "Splash Screen": undefined;
-//   Login: undefined;
-//   "Login page 2": undefined;
-//   "forgot password screen": undefined;
-// };
-
-// declare global {
-//   namespace ReactNavigation {
-//     interface RootParamList {
-//   "Splash Screen": undefined;
-//   Login: undefined;
-//   "Login page 2": undefined;
-//   "forgot password screen": undefined;
-//     }
-//   }
-// }
 
 const Stack = createNativeStackNavigator();
-// const Stack = createNativeStackNavigator<RootStackParamList>();
+
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash Screen">
-        <Stack.Screen
-          name="Splash Screen"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Login page" component={LogInScreen} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="Login page 2"
-          component={LoginScreen2}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="forgot password screen"
-          component={ForgotPassword}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName="Splash Screen"> 
+        <Stack.Screen name="Splash Screen 2" component={SplashScreen}  options={{headerShown:false}}/>
+        <Stack.Screen name="Login page" component={LogInScreen}  options={{headerShown:false}}/>
+        <Stack.Screen name="Login page 2" component={LoginScreen2}  options={{headerShown:false}} />
+        <Stack.Screen name="forgot password screen" component={ForgotPassword}  options={{headerShown:false}} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -58,10 +29,11 @@ function App() {
 
 export default App;
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
