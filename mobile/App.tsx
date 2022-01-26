58330fb37b6512ead6f02a6cbe41a11baeed8453
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2022-01-25 19:08:11 -0500

    removing styslesheet from app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StyleSheet} from "react-native";
-
+import React from "react";
 
 import ForgotPassword from "./src/screens/ForgotPassword";
 import LogInScreen from "./src/screens/LogInScreen";
