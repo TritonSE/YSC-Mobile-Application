4ba9f1d3be539ebf8aa35970856aecaeca77d98e
Akhilan Gurumoorthy <akhilan@gurumoorthy.com>
2022-02-07 17:53:09 -0800

    Username Implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,28 +7,44 @@ const PORT = process.env.PORT || 3000;
 
 // Currently locks are socket.id's. Will change to base them off of usernames
 type BoardState = {
-  lock: string;
+  lock: number;
   board: string;
+  players: string[];
 };
 const boards = new Map();
-let currRoom = 0;
+const rooms = new Map();
+let numRooms = 0;
+
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-io.on("connection", (socket:any) => {
 
-  // socket only has socket ID room => create new room
-  if(socket.rooms.size == 1) {
-    if(io.sockets.adapter.rooms.get(currRoom) == null) {
-      boards.set(currRoom, {lock: socket.id, board: ""});
-    } else if(io.sockets.adapter.rooms.get(currRoom).size == 2) {
-      currRoom++;
-      boards.set(currRoom, { lock: socket.id, board: ""});
-    }
-    socket.join(currRoom)
-  } 
+io.on("connection", (socket:any) => {
+  let username = socket.handshake.auth.username;
+  let currRoom = "Room " + numRooms;
+
+  if (rooms.get(username) != null) {
+    let newSocket = {...rooms.get(username), socket: socket.id};
+    rooms.set(username, newSocket);
+  } else {
+    let board:BoardState = {lock: 0, board: "", players: [username]};
+    // socket only has socket ID room => create new room
+    if(socket.rooms.size == 1) {
+      if(io.sockets.adapter.rooms.get(currRoom) == null) {
+        //Do Nothing
+      } else if(io.sockets.adapter.rooms.get(currRoom).size == 2) {
+        numRooms++;
+        currRoom = "Room " + numRooms;
+      } else {
+        board = {...board, players: [boards.get(currRoom).players[0], username]}
+      }
+      boards.set(currRoom, board);
+      rooms.set(username, {room: currRoom, socket:socket.id});
+      socket.join(currRoom)
+    } 
+  }
 
   // server updates board state and broadcasts the new board to the other player
   socket.on("try chess move", (newBoard:any) => {
