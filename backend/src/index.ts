fb782630d454d53e6493337defd511baac196287
Akhilan Gurumoorthy <akhilan@gurumoorthy.com>
2022-02-22 11:19:40 -0800

    Require Statement Debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,5 @@
+import type {RoomData, BoardState} from "./types";
+
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
@@ -5,21 +7,11 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
-// boards maps room to BoardState (BoardState contains all data in regards to the state of the game) 
-type BoardState = {
-  lock: number;
-  board: string;
-  players: string[];
-};
+// boards maps room to BoardState (BoardState contains all data in regards to the state of the game and is declared in types.d.ts) 
 const boards = new Map<string, BoardState>(); 
 
-// rooms maps username to RoomData (RoomData contains connection information for users)
-type RoomData = {
-  room: string;
-  socket: string;
-}
+// rooms maps username to RoomData (RoomData contains connection information for users and is declared in types.d.ts)
 const rooms = new Map<string, RoomData>(); 
-
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
@@ -35,7 +27,7 @@ io.on("connection", (socket:any) => {
   if (rooms.get(username) != null) {
 
     // If so, place socket back into that room 
-    let newSocket = {...rooms.get(username), socket: socket.id};
+    let newSocket:RoomData = {room: rooms.get(username)!.room, socket: socket.id};
     rooms.set(username, newSocket);
   } else {
     let board:BoardState = {lock: 0, board: "", players: [username]};
@@ -48,7 +40,7 @@ io.on("connection", (socket:any) => {
         if(io.sockets.adapter.rooms.get(currRoom).size == 2) { 
           currRoom = "Room " + (boards.size + 1); 
         } else {
-          board = {...board, players: [boards.get(currRoom).players[0], username]} 
+          board = {...board, players: [boards.get(currRoom)!.players[0], username]} 
         }
       }
       boards.set(currRoom, board);
@@ -57,61 +49,10 @@ io.on("connection", (socket:any) => {
     } 
   }
 
-  // CLIENT WORKFLOW FOR ATTEMPTED MOVE
-  // client A should emit "try chess move" and send the board state after the attempted move
-  // if the move is successful, both clients should handle "updated board" event with the new board being sent 
-  // otherwise, client A should handle "error message" event with an error message being sent 
-  socket.on("try chess move", (newBoard:any) => {
-    let room = rooms.get(username).room;
-    let currLock = boards.get(room).lock;
-
-    //Check if lock points to current client => update board state
-    if (boards.get(room).players[currLock] == username) {
-      let nextLock = (currLock + 1) % 2;
-      let newBoardState = {...boards.get(room), lock: nextLock, board: newBoard};
-      boards.set(room, newBoardState);
-      io.in(room).emit("updated board", newBoard);
-    } else {
-      socket.emit("error message", "It is not your turn");
-    }
-  });
-
-  // CLIENT WORKFLOW FOR RESIGNATION
-  // client A should emit "resign" if they attempt to resign
-  // all resignations are successful and both clients should handle "game resigned" event with the username of client A being sent 
-  // the server will handle deletion of rooms
-  socket.on("resign", () => {
-    let room = rooms.get(username).room;
-    io.in(room).emit("game resigned", username);
-    rooms.delete(boards.get(room).players[0]);
-    rooms.delete(boards.get(room).players[1]);
-    boards.delete(room);
-  });
-
-  // CLIENT WORKFLOW FOR DRAWING
-  // client A should emit "try draw" if they attempt to draw
-  // client B should handle "draw request" event with the username of the client A being sent
-  // if client B wants to accept the draw, client B should emit "draw accepted"
-  //  in this case, both clients should handle "game drawn" event with username of client B being sent
-  // if client B wants to decline the draw, client B should emit "draw rejected"
-  //  in this case, client A should handle "draw request rejected" with username of client B being sent
-  socket.on("try draw", () => {
-    let room = rooms.get(username).room;
-    socket.to(room).emit("draw request", username);
-  });
-
-  socket.on("draw accepted", () => {
-    let room = rooms.get(username).room;
-    io.in(room).emit("game drawn", username);
-    rooms.delete(boards.get(room).players[0]);
-    rooms.delete(boards.get(room).players[1]);
-    boards.delete(room);
-  });
-
-  socket.on("draw rejected", () => {
-    let room = rooms.get(username).room;
-    socket.to(room).emit("draw request rejected", username); 
-  });
+  require("./eventHandlers/moveHandler.ts")(socket, io, username, rooms, boards);
+  require("./eventHandlers/resignHandler.ts")(socket, io, username, rooms, boards);
+  require("./eventHandlers/drawHandler.ts")(socket, io, username, rooms, boards);
+  
 });
 
 io.of("/").adapter.on("delete-room", (room:any) => {  
---
+++ backend/src/types.ts
@@ -0,0 +1,17 @@
+type RoomData = {
+        room: string;
+        socket: string;
+};
+type BoardState = {
+        lock: number;
+        board: string;
+        players: string[];
+};
+
+export {
+    RoomData,
+    BoardState,
+
+}
+
+
