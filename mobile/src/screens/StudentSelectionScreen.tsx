1331729758104df9c6216ff0ed6b2a0abba712ed
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2022-11-08 21:46:23 -0800

    added student selection

---
+++ mobile/src/screens/StudentSelectionScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import { AppStylesheet } from "../styles/AppStylesheet";
+
+const StudentSelectionScreen = () => {
+
+  return (
+    <View style={AppStylesheet.container}>
+        <Text>3 Students Online</Text>
+    </View>
+  );
+};
+export default StudentSelectionScreen;
